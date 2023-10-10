8ccea3bcbafa28926a09abbe098dae74f6feade9
Gabriela <gabrielajsv@gmail.com>
2023-10-09 22:57:28 -0400

    deletar professores

--- src/app/views/prof-card/prof-card.module.ts
+++ src/app/views/prof-card/prof-card.module.ts
@@ -2,12 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ProfCardComponent } from './prof-card.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [ProfCardComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
-    CommonModule,CardModule
+    CommonModule,CardModule,ButtonModule
   ],
   exports: [ProfCardComponent]
 })
