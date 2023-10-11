f02445184ad5ac287d82e610f318594a69e890ff
Gabriela <gabrielajsv@gmail.com>
2023-10-10 20:19:16 -0400

    ajustes de bugs

--- src/app/views/plano-card/plano-card.module.ts
+++ src/app/views/plano-card/plano-card.module.ts
@@ -2,12 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { PlanoCardComponent } from './plano-card.component';
-
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [PlanoCardComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
-    CommonModule,CardModule
+    CommonModule,CardModule, ButtonModule
   ],
   exports: [PlanoCardComponent]
 })
